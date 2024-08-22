faab9bed9e2cd08ab427d384f2ef1ce8fde9bc69
Ahmed Mohamed Elatar <59385729+Ahmed-Elatar@users.noreply.github.com>
2024-08-22 13:59:19 +0300

    Update settings.py

--- mon/pro/pro/settings.py
+++ mon/pro/pro/settings.py
@@ -72,21 +72,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'pro.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
-# settings.py
-import mongoengine
 
 # MongoDB settings
+import mongoengine
+
 MONGODB_SETTINGS = {
     'db': 'test_11',
     'host': 'localhost',
